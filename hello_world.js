c94f2d9ce5290549fae4acd0b337d3d41825f35f
Emma Laroche <E.R.Laroche@gmail.com>
2014-09-14 15:35:17 -0500

    Add helloWorld to .js file.

--- hello_world.js
+++ hello_world.js
@@ -1,11 +1,17 @@
 function sayHello() {
-        	var greetVar = "<h1>This greeting..</h1>";
+        	var greetVar = "<h2>This greeting..</h2>";
         	var lineBreak = "<br/>";
-        	var moreGreet = "<h3>...is brought to you by a function.</h3>";
+        	var moreGreet = "<h4>    ...is brought to you by a function.</h4>";
+        	var helloWorld = "<h1>Hello World!</h1>";
 
         	document.write(greetVar);
         	document.write(lineBreak);
         	document.write(lineBreak);
         	document.write(moreGreet);
         	document.write(lineBreak);
+        	document.write(lineBreak);
+        	document.write(lineBreak);
+        	document.write(lineBreak);
+        	document.write(lineBreak);
+        	document.write(helloWorld);
         }
