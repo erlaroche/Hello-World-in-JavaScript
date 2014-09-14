56863028097d90f65ac5e2544c567dc8a6d8ec5b
Emma Laroche <E.R.Laroche@gmail.com>
2014-09-14 15:26:23 -0500

    Initial commit, Hello World simple js project.

---
+++ hello_world.js
@@ -0,0 +1,11 @@
+function sayHello() {
+        	var greetVar = "<h1>This greeting..</h1>";
+        	var lineBreak = "<br/>";
+        	var moreGreet = "<h3>...is brought to you by a function.</h3>";
+
+        	document.write(greetVar);
+        	document.write(lineBreak);
+        	document.write(lineBreak);
+        	document.write(moreGreet);
+        	document.write(lineBreak);
+        }
